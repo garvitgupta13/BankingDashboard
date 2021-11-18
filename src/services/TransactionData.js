67d45d37f9596e67cef97878328542fe1d8fab9b
garvitgupta13 <garvitgupta2001@gmail.com>
2021-11-18 12:51:53 +0530

    initialize react app

---
+++ src/services/TransactionData.js
@@ -0,0 +1,66 @@
+export const DUMMY_TRANSACTION = [
+    {
+        id: "101",
+        amount: 1500,
+        data: "21-09-2021",
+        category: "shopping",
+        payed: true,
+        stmt: "went to walmart"
+    },
+    {
+        id: "102",
+        amount: 150000,
+        data: "25-09-2021",
+        category: "shopping",
+        payed: true,
+        stmt: "baught a new bike"
+    },
+    {
+        id: "103",
+        amount: 60000,
+        data: "25-09-2021",
+        category: "shopping",
+        payed: false,
+        stmt: "sold old bike"
+    },
+    {
+        id: "104",
+        amount: 800,
+        data: "21-09-2021",
+        category: "bill",
+        payed: true,
+        stmt: "payed electricity bill"
+    },
+    {
+        id: "105",
+        amount: 65000,
+        data: "21-09-2021",
+        category: "fee",
+        payed: true,
+        stmt: "payed college fee"
+    },
+    {
+        id: "106",
+        amount: 5000,
+        data: "21-09-2021",
+        category: "fee",
+        payed: true,
+        stmt: "payed tution fee for Compuuter Science"
+    },
+    {
+        id: "106",
+        amount: 500,
+        data: "21-09-2021",
+        category: "rent",
+        payed: false,
+        stmt: "received monthly rent"
+    },
+    {
+        id: "104",
+        amount: 800,
+        data: "21-19-2021",
+        category: "payed water bill",
+        payed: true,
+        stmt: "payed electricity bill"
+    }
+];
